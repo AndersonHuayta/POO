190c598c2ab941b8780be47ae9763791d4c3b81a
AndersonHuayta <141778814+AndersonHuayta@users.noreply.github.com>
2023-09-12 09:44:26 -0500

    Add files via upload

---
+++ pkg17_java/Main.java
@@ -0,0 +1,17 @@
+
+package pkg17_java;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Persona objpersona1 = new Persona();
+        
+        objpersona1.nombre = "bryan";
+        
+        objpersona1.Persona11();
+        objpersona1.Persona11("nombre2");
+        objpersona1.edad11(17);
+    }
+    
+}
---
+++ pkg17_java/Persona.java
@@ -0,0 +1,32 @@
+
+package pkg17_java;
+
+
+public class Persona {
+
+    String nombre;
+    int edad;
+    String EstadoCivil;
+
+    public void Persona11() {
+        System.out.println("Anderson");
+    }
+    public void Persona11(String nombre) {
+        System.out.println("kevin");
+    }
+    public void edad11(int edad) {
+        System.out.println(17);
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int edad1() {
+        return 12;
+    }
+    
+    public String estado1() {
+        return EstadoCivil;
+    }
+}
+
